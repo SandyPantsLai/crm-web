ad3ec80701bf6389602bf04e8c2c37832904b624
Sandy Lai <sandywslai@gmail.com>
2015-04-08 12:11:17 -0400

    Started crm app with Contact class and dummy contact data

---
+++ crm.rb
@@ -0,0 +1,17 @@
+require_relative 'contact'
+require 'sinatra'
+
+get "/" do
+  @crm_app_name = "My CRM"
+  erb:index
+end
+
+
+get "/contacts" do
+  @contacts = []
+  @contacts << Contact.new(1, "Yehuda", "Katz", "yehuda@example.com", "Developer")
+  @contacts << Contact.new(2, "Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+  @contacts << Contact.new(3, "Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
+  erb :contacts
+end
