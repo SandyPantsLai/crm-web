b20d877a1dde5ae3a23da6cadee90a2f4c00dacc
Sandy Lai <sandywslai@gmail.com>
2015-04-10 08:09:12 -0400

    More styling

--- crm.rb
+++ crm.rb
@@ -4,23 +4,23 @@ require 'sinatra'
 
 $rolodex= Rolodex.new
 
-get "/" do
+get '/' do
   @crm_app_name = "My CRM"
-  erb:index
+  erb :index
 end
 
 
-get "/contacts" do
+get '/contacts' do
   erb :contacts
+end
+
+get '/contacts/new' do
+  erb :new_contact
 end
 
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:notes])
   $rolodex.add_new_contact(new_contact)
   redirect to('/contacts')
-end
-
-get '/contacts/new' do
-  erb :new_contact
 end
 
